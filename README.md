fa5d6ebe38814ec9b36591f61103d2ed43e226c3
rehrler <romehrler@gmail.com>
2021-11-30 11:40:31 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# aoc 21 :christmas_tree: :santa:
+
+## Advent of Code repository for year '21 in Java 17
+
+### Day 1
---
+++ src/ch/aoc21/aoc21_1.java
@@ -0,0 +1,8 @@
+package ch.aoc21;
+
+public class aoc21_1 {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
